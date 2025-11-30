079b2ebe99a192a3f72dd1761501ce0b4ad3a168
Andrij Skoropad <andross_s@ukr.net>
2025-11-30 15:45:12 +0200

    add HW

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // ❗ Не перезавантажуємо сторінку
+
+  const { email, password } = form.elements;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  // ❗ Перевірка на порожні поля
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  // ❗ Формуємо об'єкт даних
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  // ❗ Очищуємо форму
+  form.reset();
+});
